c7d9b1b93c2bd962f03e479180ee550008045c5e
Khalil <Khalil@ext-130-66-206-145-wifi.ec-nantes.fr>
2017-11-23 09:40:42 +0100

    Méthode de lecture de fichier + Début d'implémentation de la classe PGM

--- src/tp2_medev/PGM.java
+++ src/tp2_medev/PGM.java
@@ -5,12 +5,27 @@
  */
 package tp2_medev;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author Khalil
  */
 public class PGM {
+    private int largeur;
+    private int hauteur;
+    private String commentaire;
+    private String niveaux;
     
-    
-    
+    public static int tailleMax = 512;
+
+    public PGM() { //Constructeur par défaut, image toute noire
+        this.largeur = tailleMax;
+        this.hauteur = tailleMax;
+        this.commentaire = "#Nouvelle image PGM";
+    }
+
 }
--- src/tp2_medev/TraitementImage.java
+++ src/tp2_medev/TraitementImage.java
@@ -1,12 +1,31 @@
 
 package tp2_medev;
 
+import java.io.*;
+
 
 public class TraitementImage {
 
 
     public static void main(String[] args) {
         
+    }
+    
+    public static void lecture(String fichier) {
+        String chaine = "";
+        try {
+            InputStream ips = new FileInputStream(fichier);
+            InputStreamReader ipsr = new InputStreamReader(ips);
+            BufferedReader br = new BufferedReader(ipsr);
+            String ligne;
+            while ((ligne = br.readLine()) != null) {
+                System.out.println(ligne);
+                chaine += ligne + "\n";
+            }
+            br.close();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
     
     
